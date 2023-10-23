07b04aa45f813f4fea17197b00af65b8f7cba004
Arthur de Menezes <arthurmenezes554@gmail.com>
2023-10-23 16:48:00 -0300

    Category

---
+++ src/main/java/com/SpirngProject/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.SpirngProject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.SpirngProject.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
